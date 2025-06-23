e322f9419f817bd9142e6e497c1a44d8e2cba7fa
miguelcrwz <miguwlcrwz@gmail.com>
2025-06-23 15:53:56 -0300

    feat: :sparkles: add starbackground to home page layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,9 @@
-export const Home = () => {
-    return <div className="min-h-screen">
-        {/* Background Effects */}
+import { StarBackground } from "../components/StarBackground"
 
+export const Home = () => {
+    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+        {/* Background Effects */}
+        <StarBackground />
         {/* Navbar */}
 
         {/* Main Content */}
